316300f59d8702e042d8c1b4f9e249e71fe84113
chadg <gibeautc@oregonstate.edu>
2018-09-07 19:14:53 -0700

    update

--- const.go
+++ const.go
@@ -1,48 +1,43 @@
 package main
 
 import (
-	"time"
 	"fmt"
+	"time"
 )
 
-type location struct{
-	id int   //for OSM style mapping
-	lat float32		//global cords
+type location struct {
+	id  int     //for OSM style mapping
+	lat float32 //global cords
 	lon float32
-	x int			//local cords
-	y int
+	x   int //local cords
+	y   int
 }
 
-type navLogic interface{
-	decide(app *App) (*motorPair,error)
+type navLogic interface {
+	decide(app *App) (*motorPair, error)
 	showType() string
 }
 
-type motorPair struct{
+type motorPair struct {
 	motor1 int
 	motor2 int
-	time time.Time
+	time   time.Time
 }
-func(self *motorPair) printMotor() {
-	if self!=nil{
-		fmt.Printf("Motor1: %d\n",self.motor1)
-		fmt.Printf("Motor2: %d\n",self.motor2)
-	}else{
+
+func (self *motorPair) printMotor() {
+	if self != nil {
+		fmt.Printf("Motor1: %d\n", self.motor1)
+		fmt.Printf("Motor2: %d\n", self.motor2)
+	} else {
 		fmt.Println("Motor Pair is Null")
 	}
 }
 
-type sensorData struct{
-	bump1 bool
-	bump2 bool
-	bump3 bool
-	bump4 bool
-	sonar1 int
-	sonar2 int
+type sensorData struct {
+	raw  string
 	time time.Time
 }
 
-
-func stop() *motorPair{
+func stop() *motorPair {
 	return new(motorPair)
 }
--- main.go
+++ main.go
@@ -3,133 +3,88 @@ package main
 import (
 	"fmt"
 	"strconv"
-	"strings"
 	"time"
 )
 
-
-
-func main(){
+func main() {
 	var err error
-	app:=new(App)
+	app := new(App)
 	app.setup()
 	fmt.Println("Starting up")
-	toUc:=make(chan motorPair,100)
-	fromUc:=make(chan sensorData,100)
-	go SerialThread(toUc,fromUc)
+	toUc := make(chan motorPair, 100)
+	fromUc := make(chan sensorData, 100)
+	go SerialThread(toUc, fromUc)
+	for true {
 
-	for true{
-
-		if time.Now().Unix()-app.lastPrint.Unix()>1{
+		if time.Now().Unix()-app.lastPrint.Unix() > 1 {
 			app.printApp()
-			app.lastPrint=time.Now()
+			app.lastPrint = time.Now()
 		}
 		//check that channels are not being filled up
-		if len(toUc)>20{
-			fmt.Println("Channel toUc Length to high: ",len(toUc))
+		if len(toUc) > 20 {
+			fmt.Println("Channel toUc Length to high: ", len(toUc))
 		}
-		if len(fromUc)>20{
-			fmt.Println("Channel fromUc Length to high: ",len(toUc))
+		if len(fromUc) > 20 {
+			fmt.Println("Channel fromUc Length to high: ", len(toUc))
 		}
-		time.Sleep(5*time.Millisecond)
-		app.curMotor.motor2=50
-		app.curMotor.motor1=50
-		app.curMotor.time=time.Now()
-		toUc<-*app.curMotor
+		time.Sleep(5 * time.Millisecond)
+		app.curMotor.motor2 = 50
+		app.curMotor.motor1 = 50
+		app.curMotor.time = time.Now()
+		toUc <- *app.curMotor
 		select {
 		case msg := <-fromUc:
-			//fmt.Println("received message", msg)
-			delta:=time.Now().UnixNano()-msg.time.UnixNano()
-			delta=delta/1000/1000
-			if int(delta)>app.stale{
+			if msg.raw == "connected" {
+				fmt.Println("Serial Connected!")
+				app.serialConnected = true
+				continue
+			} else if msg.raw == "disconnected" {
+				app.serialConnected = false
+				continue
+			}
+			if !app.serialConnected {
+				fmt.Println("Serial Disconnected!!!!!")
+				time.Sleep(1 * time.Second)
+				continue
+			}
+			delta := time.Now().UnixNano() - msg.time.UnixNano()
+			delta = delta / 1000 / 1000
+			if int(delta) > app.stale {
 				fmt.Println("Stale data,ignoring")
-				return
+				continue
 			}
-			if msg.bump1 || msg.bump2 || msg.bump3 || msg.bump4{
-				app.collision=true
-			}
-			app.curSensor.bump1=msg.bump1
-			app.curSensor.bump2=msg.bump2
-			app.curSensor.bump3=msg.bump3
-			app.curSensor.bump4=msg.bump4
-			app.curSensor.sonar1=msg.sonar1
-			app.curSensor.sonar2=msg.sonar2
-		default:
-			fmt.Println("no message received")
+			app.curSensor = &msg
 		}
-		app.primaryDecision,err=app.primaryBrain.decide(app)
-		if err!=nil{
-			fmt.Println("Primary Error")
+		app.primaryDecision, err = app.brain.decide(app)
+		if err != nil {
+			fmt.Println("Brain Error")
 			fmt.Println(err.Error())
 		}
-		if app.secondaryBrain!=nil{
-			app.secondaryDecision,err=app.secondaryBrain.decide(app)
-			if err!=nil{
-				fmt.Println("Secondary Error")
-				fmt.Println(err.Error())
-			}
-		}
-
-		if app.tertiaryBrain!=nil{
-			app.tertiaryDecision,err=app.tertiaryBrain.decide(app)
-			if err!=nil{
-				fmt.Println("Tertiary Error")
-				fmt.Println(err.Error())
-			}
-		}
-		if app.primaryAssert{
-			app.curMotor=app.primaryDecision
-		}else{
-			app.mix()
-		}
 	}
-
-
 }
 
-
-func SerialThread(msgQueue chan motorPair,sensorQueue chan sensorData){
+func SerialThread(msgQueue chan motorPair, sensorQueue chan sensorData) {
 	//Launched as go routine. msgQueue is where main sends motor values to be sent to uC
 	//sensorQueue is where we put sensor values received from uC to go back to main
 
 	//connect to serial
 
-
-	for true{
+	//once connected, send msg back to main
+	conResp := new(sensorData)
+	conResp.raw = "connected"
+	sensorQueue <- *conResp
+	for true {
 		//need to make sure these are non-blocking
-		msg := <- msgQueue
+		msg := <-msgQueue
 		//out:=strconv.Itoa(msg.motor1)+","+strconv.Itoa(msg.motor2)
-		_=strconv.Itoa(msg.motor1)+","+strconv.Itoa(msg.motor2)
+		_ = strconv.Itoa(msg.motor1) + "," + strconv.Itoa(msg.motor2)
 		//serial.send out
 		//read serial till newline or timeout (1 sec?)
-		var resp sensorData
-		var err error
 		//actually receive this data from uC
-		rec:="F,F,F,F,10,15"
-		elem:=strings.Split(rec,",")
-		if len(elem)!=6{
-			fmt.Println("Error in data")
-			fmt.Println(elem)
-			continue
-		}
-		resp.time=time.Now()
-		resp.bump1=elem[0]=="T"
-		resp.bump2=elem[1]=="T"
-		resp.bump3=elem[2]=="T"
-		resp.bump4=elem[3]=="T"
-		resp.sonar1,err=strconv.Atoi(elem[4])
-		if err!=nil{
-			fmt.Println("Error in string -> int")
-			fmt.Println(err.Error())
-			resp.sonar1=-1
-		}
-		resp.sonar2,err=strconv.Atoi(elem[5])
-		if err!=nil{
-			fmt.Println("Error in string -> int")
-			fmt.Println(err.Error())
-			resp.sonar2=-1
-		}
-		sensorQueue<-resp
+		rec := "{\"bump1\":false,\"bump2\":false,\"bump3\":false,\"bump4\":false,\"sonar1\":10,\"sonar2\":15}"
+		resp := new(sensorData)
+		resp.raw = rec
+		resp.time = time.Now()
+		sensorQueue <- *resp
 	}
-
 }
